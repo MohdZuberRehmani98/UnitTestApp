9ee71ce60d884dd79f9406f342ce6144ff3baacb
Zuber Rehmani <zuber.rehmani@applligent.com>
2024-03-26 14:49:44 +0530

    Initial Commit

---
+++ UnitTestApp/UnitTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestAppApp.swift
+//  UnitTestApp
+//
+//  Created by Zuber Rehmani on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
