f81eee247091dfd165811a8c66595d9836ec10df
MohdZuberRehmani98 <166004312+MohdZuberRehmani98@users.noreply.github.com>
2024-04-18 18:01:42 +0530

    Update 5 ContentView.swift

--- UnitTestApp/ContentView.swift
+++ UnitTestApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world updated 4")
+            Text("Hello, world updated 5")
         }
         .padding()
     }
