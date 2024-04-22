21c9cf7b3513d4a0b78c3494de64ae24093f1afc
Zuber Rehmani <zuber.rehmani@applligent.com>
2024-04-22 16:08:22 +0530

    remove ui test casesand its target

--- UnitTestAppTests/ContentViewModel_Tests.swift
+++ UnitTestAppTests/ContentViewModel_Tests.swift
@@ -23,6 +23,7 @@ final class ContentViewModel_Tests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
         viewModel = nil
         cancellable.removeAll()
     }
